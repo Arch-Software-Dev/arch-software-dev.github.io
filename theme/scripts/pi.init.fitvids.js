fe0cf92ebb99535e2a0f76a4292e0e401be76a9e
mgrigajtis <matthew.grigajtis@gmail.com>
2021-11-09 10:02:04 -0500

    Removed theme

--- theme/scripts/pi.init.fitvids.js
+++
@@ -1,8 +0,0 @@
-/* Aura version: 1.8.7 */
-
-jQuery(function($){
-	"use strict";
-
-	$(".pi-img-w").fitVids();
-
-});
--- theme/scripts/pi.init.sample.js
+++
@@ -1,7 +0,0 @@
-/* Aura version: 1.8.7 */
-
-jQuery(function($){
-	"use strict";
-
-
-});
